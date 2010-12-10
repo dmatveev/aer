a8f312fb5cffff76d94107a648d0b789ae16e5b9
Dmitry Matveev <me@dmitrymatveev.co.uk>
2010-12-10 14:03:55 +0000

    The same.

---
+++ time-series-stuff.lisp
@@ -0,0 +1,46 @@
+(defclass value-policy ()
+  ((ranges :initform nil :reader ranges)))
+
+(defmethod print-object ((object value-policy) stream)
+  (format stream "~a" (slot-value object 'ranges)))
+
+(defgeneric encoder-class (policy)
+  (:documentation "factory method"))
+
+(defmethod encoder-class ((policy value-policy))
+  'value-encoder)
+
+(defun make-encoder-for (policy &key extractor)
+  (make-instance (encoder-class policy) :extractor extractor))
+
+(defclass value-encoder ()
+  ((extractor :initarg :extractor)))
+
+(defgeneric encode (value encoder policy)
+  (:documentation "encode the value using the specified encoder"))
+
+(defmethod encode (value (encoder value-encoder) (policy value-policy))
+  (with-slots (extractor) encoder 
+    (with-slots (ranges) policy
+      (numeric-encode value (min-value extractor ranges) (max-value extractor ranges) 0.8))))
+
+(defclass diff-encoder ()
+  ((base :initform t)))
+
+(defun encode-time-series (from-sequence into-matrix extractor policy)
+  (let ((encoder (make-encoder-for policy :extractor extractor)))
+    (loop :with i := 0 :for value :in from-sequence :do
+       (progn (setf (matrix-ref into-matrix 0 i) (encode value encoder policy))
+              (incf i)))))
+
+(defgeneric encode-forecast (fore-sequence into-array extractor policy)
+  (:documentation "TBD"))
+
+(defmethod encode-forecast (fore-sequence into-array extractor (policy value-policy))
+  (encode-forecast-impl fore-sequence into-array extractor (slot-value policy 'ranges)))
+
+(defun encode-forecast-impl (fore-sequence into-array extractor ranges)
+  (loop :with i := -1 :for each :in fore-sequence :do
+     (setf
+      (aref into-array (incf i))
+      (numeric-encode each (min-value extractor ranges) (max-value extractor ranges) 0.8))))
